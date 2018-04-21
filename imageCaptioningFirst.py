30c85a43e8d7acf73a14f57fe3a6358e5454c32c
PranavSharma97 <reachtopranav@yahoo.co.in>
2018-04-21 12:56:37 -0400

    Added image captioning files

---
+++ imageCaptioningFirst.py
@@ -0,0 +1,531 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 01:17:35 2018
+
+@author: pranavsharma
+"""
+
+from os import listdir
+from pickle import dump
+from pickle import dumps
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.inception_resnet_v2 import preprocess_input
+from keras.models import Model
+from numpy import array
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
+import string
+import sys
+import pickle
+import os.path
+from pickle import load
+import numpy as np
+import re
+import itertools
+import copy
+from numpy import argmax
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from nltk.translate.bleu_score import corpus_bleu
+
+
+def extract_features(directory):
+    model = InceptionResNetV2()
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    print(model.summary())
+
+    features = dict()
+    for name in listdir(directory):
+		# load an image from file
+        filename = directory + '/' + name
+        image = load_img(filename, target_size=(299, 299))
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+        # reshape data for the model
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        # prepare the image for the InceptionResNetV2 model
+        image = preprocess_input(image)
+        # get features
+        feature = model.predict(image, verbose=0)
+        # get image id
+        image_id = name.split('.')[0]
+        # store feature
+        features[image_id] = feature
+        print('>%s' % name)
+    return features
+
+# extract features from all images
+directory = '/Users/pranavsharma/Developer/Work/deep-learning/image-captioning/Flicker8k_Dataset'
+features = extract_features(directory)
+print('Extracted Features: %d' % len(features))
+# save to file
+dump(features, open('features.pkl', 'wb'))
+
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+
+filename = '/Users/pranavsharma/Developer/Work/deep-learning/image-captioning/Flickr8k_text/Flickr8k.token.txt'
+doc = load_doc(filename)
+
+def load_descriptions(doc):
+    mapping = dict()
+    for line in doc.split('\n'):
+        tokens = line.split()
+        if len(line) < 2:
+            continue
+        image_id, image_desc = tokens[0], tokens[1:]
+        image_id = image_id.split('.')[0]
+        image_desc = ' '.join(image_desc)
+        if image_id not in mapping:
+            mapping[image_id] = list()
+        mapping[image_id].append(image_desc)
+    return mapping
+
+descriptions = load_descriptions(doc)
+print('Loaded: %d ' % len(descriptions))
+
+def clean_descriptions(descriptions):
+    table = str.maketrans('','',string.punctuation)
+    for key, desc_list in descriptions.items():
+        for i in range(len(desc_list)):
+            desc = desc_list[i]
+            desc = desc.split()
+            desc = [word.lower() for word in desc]
+            desc = [w.translate(table) for w in desc]
+            desc = [word for word in desc if len(word)>1]
+            desc = [word for word in desc if word.isalpha()]
+            desc_list[i] = ' '.join(desc)
+            
+clean_descriptions(descriptions)
+
+def correct_spellings(descriptions):
+    for key in descriptions:
+        for desc in descriptions[key]:
+            index = descriptions[key].index(desc)
+            desc = re.sub(r"\bt-shirt\b", 'tshirt', desc)
+            desc = re.sub(r"\bt-shirts\b", 'tshirts', desc)
+            desc = re.sub(r"\bafrican-american\b", 'africanamerican', desc)
+            desc = re.sub(r"\bmerry-go-round\b", 'merrygoround', desc)
+            desc = re.sub(r"\bblader\b", 'rollerblader', desc)
+            desc = re.sub(r"\bsnow-covered\b", 'snowcovered', desc)
+            descriptions[key][index] = desc
+            
+correct_spellings(descriptions)
+
+file = open('glove.6B.300d.txt', 'r')
+text = file.read()
+file.close()
+embeddings = dict()
+for line in text.split('\n'):
+    if line != '':
+        word = line.split()[0]
+        embeddings[word] = np.asarray(line.split()[1:])
+
+sos = np.zeros((300,))
+sos[0] = 1
+eos = np.zeros((300,))
+eos[1] = 1
+unk = np.zeros((300,))
+unk[2] = 1
+embeddings['SOS'] = sos
+embeddings['EOS'] = eos
+embeddings['UNK'] = unk
+
+def replace_unknown_words(descriptions):
+    for key in descriptions.keys():
+        for desc in descriptions[key]:
+            index = descriptions[key].index(desc)
+            desc = ' '.join(['UNK' if d not in embeddings.keys() else d for d in desc.split()])
+            descriptions[key][index] = desc
+
+replace_unknown_words(descriptions)
+
+def to_vocabulary(descriptions):
+    all_desc = set()
+    for key in descriptions.keys():
+            [all_desc.update(d.split()) for d in descriptions[key]]
+    return all_desc
+
+vocabulary = to_vocabulary(descriptions)
+print('Vocabulary Size: %d' % len(vocabulary))
+
+# =============================================================================
+# changed = list()
+# for key in descriptions.keys():
+#     for d1, d2 in zip(desc2[key], descriptions[key]):
+#         cur = list()
+#         if d1 != d2:
+#             cur.append(d1)
+#             cur.append(d2)
+#             changed.append(cur)
+# =============================================================================
+            
+# =============================================================================
+# filt_dict = dict()
+# filt_dict['t-shirt'] = 0
+# filt_dict['t-shirts'] = 0
+# filt_dict['merry-go-round'] = 0
+# filt_dict['african-american'] = 0
+# filt_dict['blader'] = 0
+# filt_dict['snow-covered'] = 0
+# for el in changed:
+#     if 't-shirt' in el[1].split():
+#         filt_dict['t-shirt'] += el[1].split().count('t-shirt')
+#     if 't-shirts' in el[1].split():
+#         filt_dict['t-shirts'] += el[1].split().count('t-shirts')
+#     if 'snow-covered' in el[1].split():
+#         filt_dict['snow-covered'] += el[1].split().count('snow-covered')
+#     if 'african-american' in el[1].split():
+#         filt_dict['african-american'] += el[1].split().count('african-american')
+#     if 'blader' in el[1].split():
+#         filt_dict['blader'] += el[1].split().count('blader')
+#     if 'merry-go-round' in el[1].split():
+#         filt_dict['merry-go-round'] += el[1].split().count('merry-go-round')
+# =============================================================================
+
+
+###################### My Code #####################
+
+
+word2freq = dict()
+for key in descriptions.keys():
+        for desc in descriptions[key]:
+            for d in desc.split():
+                if d not in word2freq:
+                    word2freq[d] = 1
+                else:
+                    word2freq[d] += 1
+
+count = 0
+for word in word2freq.keys():
+    if word2freq[word] >= 1:
+        count += 1
+
+# =============================================================================
+# extra = list()
+# count = 0
+# for word in word2freq.keys():
+#     flag = 0
+#     for key in embeddings.keys():
+#         if key == word:
+#             count += 1
+#             flag = 1
+#             break
+#     if flag == 0:
+#         extra.append(word)
+#             
+# extra2freq = dict()
+# for word in extra:
+#     extra2freq[word] = word2freq[word]
+#     
+# filtered_extra = dict()
+# for word, freq in extra2freq.items():
+#     if freq > 5:
+#         filtered_extra[word] = freq
+# =============================================================================
+
+
+
+###################################################
+
+
+
+def save_descriptions(descriptions, filename):
+   lines = list()
+   for key, desc_list in descriptions.items():
+       for desc in desc_list:
+           lines.append(key + ' ' + desc)
+   data = '\n'.join(lines)
+   file = open(filename, 'w')
+   file.write(data)
+   file.close()
+   
+save_descriptions(descriptions, 'descriptions.txt')
+
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    for line in doc.split('\n'):
+        if len(line) < 1:
+            continue
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+def load_clean_descriptions(filename, dataset):
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        tokens = line.split()
+        image_id, image_desc = tokens[0], tokens[1:]
+        if image_id in dataset:
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            desc = 'SOS ' + ' '.join(image_desc) + ' EOS'
+            descriptions[image_id].append(desc)
+    return descriptions
+
+def load_photo_features(filename, dataset):
+    all_features = load(open(filename, 'rb'))
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+def create_embedding_matrix(word_to_index, embeddings):
+    embedding_matrix = np.zeros((len(word_to_index) + 1, 300))
+    for word, index in word_to_index.items():
+        embedding_matrix[index, :] = embeddings[word]
+    return embedding_matrix
+
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(desc) for desc in descriptions[key]]
+    return all_desc
+
+def create_tokenizer(descriptions):
+    lines = to_lines(descriptions)
+    tokenizer = Tokenizer(lower=False)
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+def create_sequences(tokenizer, max_length, descriptions, photos):
+    X1, X2, y = list(), list(), list()
+    for key, image_desc in descriptions.items():
+        for desc in image_desc:
+            seq = tokenizer.texts_to_sequences([desc])[0]
+            for i in range(1, len(seq)):
+                in_seq, out_seq = seq[:i], seq[i]
+                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                X1.append(photos[key][0])
+                X2.append(in_seq)
+                y.append(out_seq)
+    return array(X1), array(X2), array(y)
+
+def max_length(descriptions):
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
+def define_model(vocab_size, max_length, embedding_matrix):
+    inputs1 = Input(shape=(1536,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+    
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(input_dim=embedding_matrix.shape[0],
+                    output_dim=embedding_matrix.shape[1],
+                    mask_zero=True,
+                    weights=[embedding_matrix],
+                    trainable=False)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256, return_sequences=True)(se2)
+    se4 = Dropout(0.5)(se3)
+    se5 = LSTM(256)(se4)
+    
+    decoder1 = add([fe2, se5])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    print(model.summary())
+#    plot_model(model, to_file='model.png', show_shapes=True)
+    return model
+
+# train dataset
+ 
+# load training dataset (6K)
+filename = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_descriptions))
+# photo features
+train_features = load_photo_features('features.pkl', train)
+print('Photos: train=%d' % len(train_features))
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+word_to_index = tokenizer.word_index
+index_to_word = {index:word for word, index in word_to_index.items()}
+embedding_matrix = create_embedding_matrix(word_to_index, embeddings)
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+print('Description Length: %d' % max_length)
+# prepare sequences
+X1train, X2train, ytrain = create_sequences(tokenizer, max_length, train_descriptions, train_features)
+
+# dev dataset
+
+# load test set
+filename = 'Flickr8k_text/Flickr_8k.devImages.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+# descriptions
+test_descriptions = load_clean_descriptions('descriptions.txt', test)
+print('Descriptions: test=%d' % len(test_descriptions))
+# photo features
+test_features = load_photo_features('features.pkl', test)
+print('Photos: test=%d' % len(test_features))
+# prepare sequences
+X1test, X2test, ytest = create_sequences(tokenizer, max_length, test_descriptions, test_features)
+
+# fit model
+
+# define the model
+model = define_model(vocab_size, max_length, embedding_matrix)
+# define checkpoint callback
+filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+# fit model
+model.fit([X1train, X2train], ytrain, epochs=20, verbose=2, callbacks=[checkpoint], validation_data=([X1test, X2test], ytest))
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'SOS'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen = max_length)
+        yhat = model.predict([photo, sequence], verbose=0)
+        yhat = argmax(yhat)
+        word = word_for_id(yhat, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'EOS':
+            break
+    return in_text
+        
+def evaluate_model(model, descriptions, photos, tokenizer, max_length):
+    actual, predicted = list(), list()
+    for key, desc_list in descriptions.items():
+        yhat = generate_desc(model, tokenizer, photos[key], max_length)
+        references = [d.split() for d in desc_list]
+        actual.append(references)
+        predicted.append(yhat.split())
+    print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+    print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+    print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+    print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))  
+    
+# prepare tokenizer on train set
+
+# load training dataset (6K)
+filename = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_descriptions))
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+print('Description Length: %d' % max_length)
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
+# prepare test set
+
+# load test set
+filename = 'Flickr8k_text/Flickr_8k.testImages.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+# descriptions
+test_descriptions = load_clean_descriptions('descriptions.txt', test)
+print('Descriptions: test=%d' % len(test_descriptions))
+# photo features
+test_features = load_photo_features('features.pkl', test)
+print('Photos: test=%d' % len(test_features))
+
+# load the model
+filename = 'model-ep005-loss3.540-val_loss3.735.h5'
+model = load_model(filename)
+# evaluate model
+evaluate_model(model, test_descriptions, test_features, tokenizer, max_length)
+    
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+max_length = 34
+
+# load the model
+model = load_model('model-ep005-loss3.540-val_loss3.735.h5')
+
+def extract_features(filename):
+    model = InceptionResNetV2()
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+
+	# load an image from file
+    image = load_img(filename, target_size=(299, 299))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the InceptionResNetV2 model
+    image = preprocess_input(image)
+    # get features
+    feature = model.predict(image, verbose=0)
+    return feature
+
+# load and prepare the photograph
+photo = extract_features('example.jpg')
+# generate description
+description = generate_desc(model, tokenizer, photo, max_length)
+print(description)
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
